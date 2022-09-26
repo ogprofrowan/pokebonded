2ed134b8c0b7e6e8959a71f6c34fe5ecc4dd975e
ogprofrowan <ogrowan@outlook.com>
2022-09-26 18:33:36 -0400

    Add dialog mugshots

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -55,6 +55,7 @@
 #include "constants/vars.h"
 #include "constants/weather.h"
 #include "constants/quests.h"
+#include "constants/mugshots.h"
 	.include "asm/macros.inc"
 	.include "asm/macros/event.inc"
 	.include "constants/constants.inc"
